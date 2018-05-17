244f3422a4ae1160bc993d88f05d1c709bf126bd
Andréa Cursino <ascursino@gmail.com>
2018-05-17 19:23:07 +1200

    Add project files.

---
+++ DBS.InternacaoVisita/Layouts/DBS.InternacaoVisita/ExibeVisita.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Import Namespace="Microsoft.SharePoint.ApplicationPages" %>
+<%@ Register Tagprefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=3.5.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ExibeVisita.aspx.cs" Inherits="DBS.InternacaoVisita.Layouts.DBS.InternacaoVisita.ExibeVisita" DynamicMasterPageFile="~masterurl/default.master" %>
+
+<asp:Content ID="Main" ContentPlaceHolderID="PlaceHolderMain" runat="server">
+
+       <asp:Label runat="server" ID = "lblErro" ForeColor="Red"></asp:Label>
+
+        <p class="titulo_resumo">:: Acompanhamento de Visitas</p>
+        <br />
+
+		<table border="0" cellspacing="0" width="100%">
+			<tr>
+				<td colspan="2" style="height: 26px">
+					<span class="ms-formdescription"><b>Prontuário: </b><asp:Label runat="server" ID="lblProntuario" /></span><br /><br />
+                    <span class="ms-formdescription"><b>Paciente: </b><asp:Label runat="server" ID="lblPaciente" /></span>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2">&nbsp;</td>
+			</tr>
+			<tr>
+				<td width="100px" valign="top" class="ms-formlabel">
+					<b>Médico da Visita</b>
+				</td>
+				<td width="100px" valign="top" class="ms-formlabel">
+					<b>Especialidade</b>
+				</td>
+                <td width="100px" valign="top" class="ms-formlabel">
+					<b>Tipo de Visita</b>
+				</td>
+                <td width="100px" valign="top" class="ms-formlabel">
+					<b>Entrada</b>
+				</td>
+                <td width="100px" valign="top" class="ms-formlabel">
+					<b>Saída</b>
+				</td>
+                <td width="100px" valign="top" class="ms-formlabel">
+					<b>Observação</b>
+				</td>
+			</tr>
+			
+            <asp:Literal ID="ltrListaVisita" runat="server" />
+
+			</table>
+
+
+</asp:Content>
---
+++ DBS.InternacaoVisita/Layouts/DBS.InternacaoVisita/ExibeVisita.aspx.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.WebControls;
+
+namespace DBS.InternacaoVisita.Layouts.DBS.InternacaoVisita
+{
+    public partial class ExibeVisita : LayoutsPageBase
+    {
+        SPWeb web = SPContext.Current.Site.RootWeb;
+
+        int intIDInternacao = 1;
+        string strProntuario = "";
+        string strPaciente = "";
+
+        string strEspecialidade = ""; 
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                RecuperaDadosInternacao(intIDInternacao);
+
+                MontaListaVisita(intIDInternacao);
+            }
+            catch (Exception ex)
+            {
+                lblErro.Text = ex.ToString();
+            }
+        }
+
+        public void RecuperaDadosInternacao(int varIDInternacao)
+        {
+            //Acessando a lista Visita
+            SPList lstInternacao = web.Lists["Internações"];
+
+            //Construindo a Query CAML
+            SPQuery oQuery = new SPQuery();
+            oQuery.Query = "<Where>" +
+                                "<Eq>" +
+                                    "<FieldRef Name='ID' /><Value Type='Number'>" + intIDInternacao + "</Value>" +
+                                "</Eq>" +
+                           "</Where>";
+
+            //Criando uma coleção de itens, utilizando a Query CAML
+            SPListItemCollection collListItems = lstInternacao.GetItems(oQuery);
+
+            //Recupera o item
+            foreach (SPListItem oListItem in collListItems)
+            {
+                //Guarda resultado nas variáveis
+                strProntuario = SPEncode.HtmlEncode(oListItem["Prontu_x00e1_rio"].ToString());
+                strPaciente = SPEncode.HtmlEncode(oListItem["Paciente"].ToString());
+            }
+        }
+
+
+        public void MontaListaVisita(int varIDInternacao)
+        {
+            string varLinha = "";
+
+            lblProntuario.Text = strProntuario.ToString();
+            lblPaciente.Text = strPaciente.ToString();
+
+            //Acessando a lista Visita
+            SPList lstVisita = web.Lists["Visitas"];
+
+            //Construindo a Query CAML
+            SPQuery oQuery = new SPQuery();
+
+            oQuery.Query = "<Where>" +
+                                "<Eq>" +
+                                    "<FieldRef Name='Registro_x0020_de_x0020_interna_' /><Value Type='Lookup'>" + intIDInternacao + "</Value>" +
+                                "</Eq>" +
+                            "</Where>";
+
+            //Criando uma coleção de itens, utilizando a Query CAML
+            SPListItemCollection collListItems = lstVisita.GetItems(oQuery);
+
+            //Recupera o item
+            foreach (SPListItem oListItem in collListItems)
+            {
+                //Busca Especialidade do médico da visita
+                string strEspecMedico = RecuperaDadosMedico(SPEncode.HtmlEncode(oListItem["M_x00e9_dico_x0020_da_x0020_Visi"].ToString()));
+
+                //Monta linha da grid com os resultados
+                varLinha += "<tr><td valign='top' class='ms-formbody'>" + oListItem["M_x00e9_dico_x0020_da_x0020_Visi"] != null ? SPEncode.HtmlEncode(oListItem["M_x00e9_dico_x0020_da_x0020_Visi"].ToString()) : "" + "</td>" +
+                            "<td valign='top' class='ms-formbody'>" + SPEncode.HtmlEncode(strEspecMedico.ToString()) + "</td>" +
+                            "<td valign='top' class='ms-formbody'>" + oListItem["Tipo_x0020_de_x0020_Visita"] != null ? SPEncode.HtmlEncode(oListItem["Tipo_x0020_de_x0020_Visita"].ToString()) : "" + "</td>" +
+                            "<td valign='top' class='ms-formbody'>" + oListItem["Hora_x0020_de_x0020_In_x00ed_cio"] != null ? SPEncode.HtmlEncode(oListItem["Hora_x0020_de_x0020_In_x00ed_cio"].ToString()) : "" + "</td>" +
+                            "<td valign='top' class='ms-formbody'>" + oListItem["Hora_x0020_de_x0020_Fim_x0020_da"] != null ? SPEncode.HtmlEncode(oListItem["Hora_x0020_de_x0020_Fim_x0020_da"].ToString()) : "" + "</td>" +
+                            "<td valign='top' class='ms-formbody'>" + oListItem["Observa_x00e7__x00e3_o"] != null ? SPEncode.HtmlEncode(oListItem["Observa_x00e7__x00e3_o"].ToString()) : "" + "</td></tr>";
+            }
+
+            ltrListaVisita.Text = varLinha.ToString();
+        }
+
+
+        public string RecuperaDadosMedico(string strMedico)
+        {
+
+            strMedico = strMedico.Substring(strMedico.IndexOf("#") + 1);
+
+            string strEspecialidade = "";
+
+            //Acessando a lista Visita
+            SPList lstMedico = web.Lists["Medicos"];
+
+            //Construindo a Query CAML
+            SPQuery oQuery = new SPQuery();
+            oQuery.Query = "<Where>" +
+                                "<Eq>" +
+                                    "<FieldRef Name='Title' /><Value Type='Text'>" + strMedico + "</Value>" +
+                                "</Eq>" +
+                           "</Where>";
+
+
+            //Criando uma coleção de itens, utilizando a Query CAML
+            SPListItemCollection collListItems = lstMedico.GetItems(oQuery);
+
+            //Recupera o item
+            foreach (SPListItem oListItem in collListItems)
+            {
+                strEspecialidade = oListItem["Especialidade"].ToString() != null ? SPEncode.HtmlEncode(oListItem["Especialidade"].ToString()) : "";
+            }
+
+            return strEspecialidade;
+        }
+    }
+}
